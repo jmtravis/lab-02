22e453e563aead4a31c986feca6d2a835652430a
jmtravis <122946408+jmtravis@users.noreply.github.com>
2023-02-06 13:20:12 -0800

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -18,18 +18,18 @@ void error(const char *msg)
 int main(int argc, char *argv[])
 {
     /* 1. What is argc and *argv[]?
-     *
+     *     argc is argument count and argv is argument vector
      */
     int sockfd, newsockfd, portno;
     /* 2. What is a UNIX file descriptor and file descriptor table?
-     *
+     *  A UNIX file descriptor is s a process-unique identifier (handle) for a file or other input/output resource, such as a pipe or network socket.
+     * A file descriptor table is a table collection of  integer used by a process to identify an open file. 
      */
     socklen_t clilen;
 
     struct sockaddr_in serv_addr, cli_addr;
     /* 3. What is a struct? What's the structure of sockaddr_in?
-     *
-     */
+     *   A struct is used to group several related variables into one place. The sockaddr_in structure has sin_family.
     
     int n;
     if (argc < 2) {
@@ -39,7 +39,8 @@ int main(int argc, char *argv[])
     
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     /* 4. What are the input parameters and return value of socket()
-     *
+     *  The input parameters are domain(specifies the address family), type and protocal. On success, a file descriptor for the new socket is returned.  On
+       error, -1 is returned, and errno is set to indicate the error.
      */
     
     if (sockfd < 0) 
@@ -54,7 +55,7 @@ int main(int argc, char *argv[])
              sizeof(serv_addr)) < 0) 
              error("ERROR on binding");
     /* 5. What are the input parameters of bind() and listen()?
-     *
+     *   The input parameters of bind() are socket, address, and address_len. The input parameters of listen() are socket descriptor and backlog. 
      */
     
     listen(sockfd,5);
@@ -62,7 +63,7 @@ int main(int argc, char *argv[])
     
     while(1) {
         /* 6.  Why use while(1)? Based on the code below, what problems might occur if there are multiple simultaneous connections to handle?
-        *
+        *   Causes an infinite loop until a certain action is completed. The simulataneous connections are handled one by one.  
         */
         
 	char buffer[256];
@@ -70,7 +71,8 @@ int main(int argc, char *argv[])
                     (struct sockaddr *) &cli_addr, 
                     &clilen);
 	/* 7. Research how the command fork() works. How can it be applied here to better handle multiple connections?
-         * 
+         *  Fork system call is used for creating a new process, which is called child process, which runs concurrently with the process that makes the fork() call (parent process). 
+	 * After a new child process is created, both processes will execute the next instruction following the fork() system call. Fork creates a new process and allows for each new connection for a new process. 
          */
         
 	if (newsockfd < 0) 
@@ -91,5 +93,5 @@ int main(int argc, char *argv[])
 }
   
 /* This program makes several system calls such as 'bind', and 'listen.' What exactly is a system call?
- *
+ *   A system call is a request for service that a program makes of the kernel.
  */
